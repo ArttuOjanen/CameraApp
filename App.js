d467f8caf4f6a6721d6d6023caacc236901727b9
ArttuOjanen <arttu.ojanen1@gmail.com>
2022-05-10 15:56:21 +0300

    second push

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CamComponent from './Screens.js/Camera';
+import CaptureImg from './components/CaptureImg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CamComponent/>
+      
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/CaptureImg.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Input, View, StyleSheet, Button } from 'react-native';
+
+export default function CaptureImg() {
+
+    return(
+        <View>
+        <Button title="A"></Button>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  image: {
+
+  },
+});
